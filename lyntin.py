dc1b7d516025e0477b9ce59b99a76e592e3a61d6
Nate <37554478+servusDei2018@users.noreply.github.com>
2020-09-30 19:03:33 -0400

    Update lyntin.py

--- lyntin.py
+++ lyntin.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
-#######################################################################
+'''
 # This file is part of Lyntin.
 # copyright (c) Free Software Foundation 2001, 2020
 #
 # Lyntin is distributed under the GNU General Public License license.
 # See the file LICENSE for distribution details.
-#######################################################################
+'''
 bootoptions = {
     "ui": "tk",
     "datadir": "",
